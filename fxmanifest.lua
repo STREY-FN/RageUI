e1211e59a50dd491fe40432d96181423c7e2cea2
STREY-FN <145247644+STREY-FN@users.noreply.github.com>
2023-10-31 11:33:56 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version('cerulean')
+game('gta5')
+lua54('yes')
+
+client_scripts({
+    'RageUI/RMenu.lua',
+    'RageUI/menu/RageUI.lua',
+    'RageUI/menu/Menu.lua',
+    'RageUI/menu/MenuController.lua',
+    'RageUI/components/*.lua',
+    'RageUI/menu/elements/*.lua',
+    'RageUI/menu/items/*.lua',
+    'RageUI/menu/panels/*.lua',
+    'RageUI/menu/windows/*.lua',
+})
